c66125ae34064a2e541aa722287fd7f9c730b41a
jowi-dev <joey8williams@gmail.com>
2025-01-11 16:35:13 -0500

    downgrade elixir

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule AbstractEmporium.MixProject do
     [
       app: :abstract_emporium,
       version: "0.1.0",
-      elixir: "~> 1.18",
+      elixir: "~> 1.17",
       start_permanent: Mix.env() == :prod,
       escript: [main_module: AbstractEmporium, name: "emporium", include_priv_for: [AbstractEmporium]],
       deps: deps()
